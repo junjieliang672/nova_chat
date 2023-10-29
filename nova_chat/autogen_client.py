192407936c7070426913279a3d93fcbf4c86b7ac
junjieliang672 <jokitlueng@hotmail.com>
2023-10-29 18:46:31 -0400

    api base

--- nova_chat/autogen_client.py
+++ nova_chat/autogen_client.py
@@ -8,7 +8,7 @@ from nova_chat.constants import (
 config_list = [
     {
         # "api_key": get_url(ip=AIIP, port=PORT)
-        "api_key": "http://0.0.0.0:8000"
+        "api_base": "http://0.0.0.0:8000"
     }
 ]
 
